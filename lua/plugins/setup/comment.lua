fdc2e9c5273cc0ad09335b03d9e8a3cd80b99d98
Vitaly Arbuzov <Vitaly.Arbuzov@gmail.com>
2023-05-13 10:56:15 -0700

    Update comment mappings

--- lua/plugins/setup/comment.lua
+++ lua/plugins/setup/comment.lua
@@ -1,9 +1,11 @@
-require('Comment').setup()
-
--- Toggle line-wise comment
-vim.api.nvim_set_keymap('n', '<C-_>', ':normal gcc<CR>j', {noremap = true})
-vim.api.nvim_set_keymap('v', '<C-_>', ':normal gcc<CR>', {noremap = true})
-
--- Toggle block-wise comment
--- vim.api.nvim_set_keymap('n', '<TBD>', ':normal gbc<CR>', {noremap = true})
--- vim.api.nvim_set_keymap('v', '<TBD>', ':normal gb<CR>', {noremap = true})
+require('Comment').setup({
+    ignore = '^$',
+    toggler = {
+        line = '<C-_>', -- <C-/> for single line comment in normal mode
+        block = '<leader>/', -- <leader>/ for block comment in normal mode
+    },
+    opleader = {
+        line = '<C-_>', -- <C-/> to comment each line in visual mode
+        block = '<leader>/', -- <leader>/ for block comment in visual mode
+    },
+})
