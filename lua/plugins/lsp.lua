e127e8573ba5e726b04ba67d9726dde9c9557e21
Vitaly Arbuzov <arvit@amazon.com>
2023-05-16 14:42:27 -0700

    Update rename shortcut to leader+r

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,8 +64,8 @@ return {
 			local on_attach = function(client, bufnr)
 				local lsp_map = require("helpers.keys").lsp_map
 				local ts = require("telescope.builtin")
-				lsp_map("n", "<C-r>", vim.lsp.buf.rename, bufnr, "Rename symbol")
-				lsp_map("n", "<leader>la", vim.lsp.buf.code_action, bufnr, "Code action")
+				lsp_map("n", "<leader>r", vim.lsp.buf.rename, bufnr, "Rename symbol")
+				lsp_map("n", "<leader><space>", vim.lsp.buf.code_action, bufnr, "Code action")
 				lsp_map("n", "<leader>ld", vim.lsp.buf.type_definition, bufnr, "Type definition")
 				lsp_map("n", "<C-p>", vim.lsp.buf.signature_help, bufnr, "Signature help")
 				lsp_map("n", "gd", vim.lsp.buf.definition, bufnr, "Goto Definition")
