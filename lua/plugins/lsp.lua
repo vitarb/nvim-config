9c412c7691a43427f3c817d0f1b4182bbfc4fba0
Vitaly Arbuzov <Vitaly.Arbuzov@gmail.com>
2023-05-20 23:19:12 -0700

    Change error sign

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,7 +20,7 @@ return {
 			require("fidget").setup()
 
 			-- Set up cool signs for diagnostics
-			local signs = { Error = "☒", Warn = "⚠", Hint = " ", Info = " " }
+			local signs = { Error = "✗", Warn = "⚠", Hint = " ", Info = " " }
 			for type, icon in pairs(signs) do
 				local hl = "DiagnosticSign" .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
