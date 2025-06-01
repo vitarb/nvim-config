40c9bb77f4eb1a702f23d4592d3aaac9268bcda7
Vitaly <Vitaly.Arbuzov@gmail.com>
2025-06-01 10:16:48 -0700

    feat(none-ls): migrate null-ls to none-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"jose-elias-alvarez/null-ls.nvim",
+        {
+                "nvimtools/none-ls.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			local null_ls = require("null-ls")
+                        local null_ls = require("none-ls")
 			null_ls.setup({
 				sources = {
 					-- Automatically registered by mason-null-ls
