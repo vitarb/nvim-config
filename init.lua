05b9ad328dbe3d60bd150ec5bfda708340f1cbf8
Vitaly <Vitaly.Arbuzov@gmail.com>
2025-06-03 15:03:52 -0700

    Update ShellCheck and CI

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+local root = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h")
+vim.opt.rtp:prepend(root)
 vim.g.mapleader = "'"
 vim.g.maplocalleader = "'"
 if vim.env.NVIM_OFFLINE_BOOT == "1" then
